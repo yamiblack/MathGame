6c50662eaa93fe16c2bc7ce46233176ed3cad525
yamiblack <yamiblack95@gmail.com>
2020-05-04 18:35:05 +0900

    making 'Difficulty Pop-up'

--- app/src/main/java/hitesh/asimplegame/IntroPage.java
+++ app/src/main/java/hitesh/asimplegame/IntroPage.java
@@ -2,8 +2,11 @@ package hitesh.asimplegame;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,11 +15,11 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class IntroPage extends Activity {
-    Button Game_b;
-    Button Mypage_b;
-    Button Settings_b;
-    Button Rank_b;
-    ImageButton login_b;
+    Button btnGame;
+    Button btnMypage;
+    Button btnSettings;
+    Button btnRanking;
+    ImageButton btnSignin;
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
@@ -28,23 +31,14 @@ public class IntroPage extends Activity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.intropage);
 
-            Game_b = (Button) findViewById(R.id.button4);
-            Mypage_b = (Button) findViewById(R.id.button5);
-            Settings_b = (Button) findViewById(R.id.button6);
-            Rank_b = (Button) findViewById(R.id.button);
-            login_b = (ImageButton) findViewById(R.id.imageButton2);             //이미지버튼?
+            btnGame = (Button) findViewById(R.id.btn_game);
+            btnMypage = (Button) findViewById(R.id.btn_mypage);
+            btnSettings = (Button) findViewById(R.id.btn_settings);
+            btnRanking = (Button) findViewById(R.id.btn_ranking);
+            btnSignin = (ImageButton) findViewById(R.id.btn_signin);             //이미지버튼?
 
 
-            login_b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //화면전환할 클래스명을 입력한다.
-                    Intent intent1 = new Intent(getApplicationContext(),DifficultyPopup.class);
-                    startActivity(intent1);
-                }
-            });
-
-            Game_b.setOnClickListener(new View.OnClickListener() {
+            btnSignin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     //화면전환할 클래스명을 입력한다.
@@ -53,16 +47,47 @@ public class IntroPage extends Activity {
                 }
             });
 
-            Rank_b.setOnClickListener(new View.OnClickListener() {
+
+
+//            btn_game.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    AlertDialog.Builder builder = new AlertDialog.Builder((IntroPage.this));
+//                    builder.setTitle("Select Difficulty");
+//
+//
+//                    builder.setNeutralButton("Easy", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            Intent intent1 = new Intent(getApplicationContext(), QuestionActivity.class);
+//                        }
+//                    });
+//
+//                    AlertDialog alertDialog = builder.create();
+//                    alertDialog.show();
+//                }
+//            });
+
+            btnGame.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //화면전환할 클래스명을 입력한다.
+
+                    Intent intent1 = new Intent(getApplicationContext(),DifficultyPopup.class);
+                    startActivity(intent1);
+                }
+            });
+
+            btnRanking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //화면전환할 클래스명을 입력한다.
-                Intent intent1 = new Intent(getApplicationContext(),DifficultyPopup.class);
+                Intent intent1 = new Intent(getApplicationContext(), DifficultyPopup.class);
                 startActivity(intent1);
             }
             });
 
-        Mypage_b.setOnClickListener(new View.OnClickListener() {
+        btnMypage.setOnClickListener(new View.OnClickListener() {
                 @Override
         public void onClick(View view) {
             //화면전환할 클래스명을 입력한다.
@@ -72,7 +97,7 @@ public class IntroPage extends Activity {
         });
 
 
-        Settings_b.setOnClickListener(new View.OnClickListener() {
+        btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //화면전환할 클래스명을 입력한다.
@@ -84,7 +109,7 @@ public class IntroPage extends Activity {
     }
 
     public void mOnPopupClick(View view) {
-        Intent intent = new Intent(this,DifficultyPopup.class);
+        Intent intent = new Intent(this,QuestionActivity.class);
         startActivityForResult(intent, 1);
     }
 }

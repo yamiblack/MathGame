4fc70c6c4241abd6089835a6507d0eb5f1321260
yamiblack <yamiblack95@gmail.com>
2020-05-05 16:27:40 +0900

    working on 'Sign-up'

--- app/src/main/java/hitesh/asimplegame/SignUpActivity.java
+++ app/src/main/java/hitesh/asimplegame/SignUpActivity.java
@@ -1,10 +1,11 @@
 package hitesh.asimplegame;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {

f5cb039f0e9b9f698f5f13046b46dc11f74b4a72
SEO-J17 <59912150+SEO-J17@users.noreply.github.com>
2020-05-07 05:08:25 +0900

    Update MusicService.java

--- app/src/main/java/hitesh/asimplegame/MusicService.java
+++ app/src/main/java/hitesh/asimplegame/MusicService.java
@@ -1,36 +1,42 @@
-//package hitesh.asimplegame;
-//import android.app.Service;
-//import android.content.Intent;
-//import android.media.MediaPlayer;
-//import android.os.IBinder;
-//import android.util.Log;
-//
-//public class MusicService extends Service {
-//    private static final String TAG = "MusicService";
-//    public MediaPlayer mp;
-//
-//    public IBinder onBind(Intent intent){
-//
-//        return null;
-//    }
-//
-//    public void onCreate(){
-//        super.onCreate();
-//        Log.d(TAG,"onCreate");
-//        mp = MediaPlayer.create(this, R.raw.bgm111);
-//        mp.setLooping(false);
-//    }
-//
-//    public void  onDestory(){
-//        super.onDestroy();
-//        Log.d(TAG,"onDestory");
-//        mp.stop();
-//    }
-//
-//    public int onStartCommand(Intent intent, int flags, int startld){
-//        Log.d(TAG, "onStart");
-//        mp.start();
-//        return super.onStartCommand(intent, flags, startld);
-//    }
-//
-//}
+// package hitesh.asimplegame;
+// import android.app.Service;
+// import android.content.Intent;
+// import android.media.MediaPlayer;
+// import android.os.IBinder;
+// import android.util.Log;
+
+// public class MusicService extends Service {
+//     MediaPlayer mp;
+
+//     @Override
+//     public IBinder onBind(Intent intent) {
+//         return null;
+//     }
+
+
+//     @Override
+//     public void onCreate() {
+//         super.onCreate();
+
+
+//         mp = MediaPlayer.create(this, R.raw.bgm111);
+//         mp.setLooping(false);
+//     }
+
+
+//     @Override
+//     public int onStartCommand(Intent intent, int flags, int startId) {
+//         mp.start();
+//         return super.onStartCommand(intent, flags, startId);
+//     }
+
+//     @Override
+//     public void onDestroy() {
+//         super.onDestroy();
+//         mp.stop();
+
+//     }
+// }
+
+
+

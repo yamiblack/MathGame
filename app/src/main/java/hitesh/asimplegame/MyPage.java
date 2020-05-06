3064fc8598b40456944c029d4c8d8e68e1de9920
SEO-J17 <59912150+SEO-J17@users.noreply.github.com>
2020-05-07 05:05:13 +0900

    Mypage

--- app/src/main/java/hitesh/asimplegame/MyPage.java
+++ app/src/main/java/hitesh/asimplegame/MyPage.java
@@ -1,5 +1,32 @@
 package hitesh.asimplegame;
 
-class MyPage {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MyPage extends Activity {
+    private TextView name_view,email_view,id_view;
+
+    @Override
+    protected  void onCreate(Bundle savedinstancState) {
+        super.onCreate(savedinstancState);
+        setContentView(R.layout.mypage);
+
+        name_view =(TextView) findViewById(R.id.textView12);
+        email_view =(TextView) findViewById(R.id.textView11);
+        id_view =(TextView) findViewById(R.id.textView10);
+
+        Intent intent = getIntent() ;
+        //로그인 액티비티에서 이름 맞출것.
+        String userID = intent.getStringExtra("userID");
+        String useremail = intent.getStringExtra("userEmail");
+        String username = intent.getStringExtra("userName");
+
+        name_view.setText(userID);
+        email_view.setText(useremail);
+        name_view.setText(username);
+    }
 
 }

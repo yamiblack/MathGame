28d60b2dd144e4c959999904c437c2a65d43e920
yamiblack <yamiblack95@gmail.com>
2020-05-07 23:43:36 +0900

    added 'sound'

--- app/src/main/java/hitesh/asimplegame/GameDifficultyPopup.java
+++ app/src/main/java/hitesh/asimplegame/GameDifficultyPopup.java
@@ -2,6 +2,8 @@ package hitesh.asimplegame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -19,7 +21,9 @@ public class GameDifficultyPopup extends Activity {
     Button btnPractice;
     Button btnChallenge;
     Button btnClose;
-    String email;
+
+    SoundPool soundpool;
+    int soundid;
 
 
     @Override
@@ -32,8 +36,10 @@ public class GameDifficultyPopup extends Activity {
         layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         layoutParams.dimAmount = 0.7f;
         getWindow().setAttributes(layoutParams);
-
         setContentView(R.layout.difficultypopup_activity);
+
+        soundpool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
+        soundid = soundpool.load(this,R.raw.sound_button,1);
 
         btnEasy = (Button) findViewById(R.id.btn_easy);
         btnMedium = (Button) findViewById(R.id.btn_medium);
@@ -41,11 +47,19 @@ public class GameDifficultyPopup extends Activity {
         btnPractice = (Button) findViewById(R.id.btn_practice);
         btnChallenge = (Button) findViewById(R.id.btn_challenge);
         btnClose = (Button) findViewById(R.id.btn_close);
+        //기본버튼음제거
+        btnEasy.setSoundEffectsEnabled(false);
+        btnMedium.setSoundEffectsEnabled(false);
+        btnHard.setSoundEffectsEnabled(false);
+        btnPractice.setSoundEffectsEnabled(false);
+        btnChallenge.setSoundEffectsEnabled(false);
+        btnClose.setSoundEffectsEnabled(false);
 
 
         btnEasy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 setRandomDB();
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -57,6 +71,7 @@ public class GameDifficultyPopup extends Activity {
         btnMedium.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 setRandomDB();
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -68,6 +83,7 @@ public class GameDifficultyPopup extends Activity {
         btnHard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 setRandomDB();
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -79,6 +95,7 @@ public class GameDifficultyPopup extends Activity {
         btnPractice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 setRandomDB();
                 Intent intent = new Intent(getApplicationContext(), PracticeActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -90,6 +107,7 @@ public class GameDifficultyPopup extends Activity {
         btnChallenge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 setRandomDB();
                 Intent intent = new Intent(getApplicationContext(), ChallengeActivity.class);
 
@@ -103,6 +121,7 @@ public class GameDifficultyPopup extends Activity {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 GameDifficultyPopup.super.onBackPressed();
             }
         });
--- app/src/main/java/hitesh/asimplegame/MainPageActivity.java
+++ app/src/main/java/hitesh/asimplegame/MainPageActivity.java
@@ -3,6 +3,8 @@ package hitesh.asimplegame;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -17,57 +19,82 @@ public class MainPageActivity extends Activity {
     Button btnRanking;
     Button btnSignOut;
 
+    SoundPool soundpool;
+    int soundid;
+
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-        @SuppressLint("WrongViewCast")
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.mainpage);
+    @SuppressLint("WrongViewCast")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+
+        soundpool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
+        soundid = soundpool.load(this,R.raw.sound_button,1);
 
 
-            if(FirebaseAuth.getInstance().getCurrentUser() == null) {
-                startSignInActivity();
-            }
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startSignInActivity();
+        }
 
 
-            btnGame = (Button) findViewById(R.id.btn_game);
-            btnMypage = (Button) findViewById(R.id.btn_mypage);
-            btnSettings = (Button) findViewById(R.id.btn_settings);
-            btnRanking = (Button) findViewById(R.id.btn_ranking);
-            btnSignOut = (Button) findViewById(R.id.btn_signout);
+        btnGame = (Button) findViewById(R.id.btn_game);
+        btnMypage = (Button) findViewById(R.id.btn_mypage);
+        btnSettings = (Button) findViewById(R.id.btn_settings);
+        btnRanking = (Button) findViewById(R.id.btn_ranking);
+        btnSignOut = (Button) findViewById(R.id.btn_signout);
 
-            btnSignOut.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    FirebaseAuth.getInstance().signOut();
-                    startSignInActivity();
-                }
-            });
+        btnGame.setSoundEffectsEnabled(false);
+        btnMypage.setSoundEffectsEnabled(false);
+        btnRanking.setSoundEffectsEnabled(false);
+        btnSignOut.setSoundEffectsEnabled(false);
+        btnSettings.setSoundEffectsEnabled(false);
 
-            btnGame.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
 
-                    startActivity(new Intent(getApplicationContext(), GameDifficultyPopup.class));
-                }
-            });
-
-            btnRanking.setOnClickListener(new View.OnClickListener() {
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
+                FirebaseAuth.getInstance().signOut();
+                startSignInActivity();
+            }
+        });
 
+
+
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
+                startActivity(new Intent(getApplicationContext(), GameDifficultyPopup.class));
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
+                startActivity(new Intent(getApplicationContext(), Setting.class));
+            }
+        });
+
+        btnRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
                 startActivity(new Intent(getApplicationContext(), RankingActivity.class));
             }
-            });
+        });
 
         btnMypage.setOnClickListener(new View.OnClickListener() {
-                @Override
-        public void onClick(View view) {
-                    startActivity(new Intent(getApplicationContext(), MyPageActivity.class));
-        }
+            @Override
+            public void onClick(View view) {
+                soundpool.play(soundid,1f,1f,0,0,1f);
+                startActivity(new Intent(getApplicationContext(), MyPageActivity.class));
+            }
         });
 
     }
